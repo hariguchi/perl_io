d89dc7872304b1626a3ef2d1b6d56cc793741a3e
Yoichi Hariguchi <yoichi@Yoichis-Mac-2.local>
2016-03-29 10:30:23 -0700

    Fixed a bug in close()

--- perl_io.py
+++ perl_io.py
@@ -1,19 +1,24 @@
 r''' perl_io - Opens a file or pipe in the Perl style
 
-   Copyright (c) 2016 Yoichi Hariguchi
-   All rights reserved.
+Copyright (c) 2016 Yoichi Hariguchi
 
-   Permission to use, copy, modify, and distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 
 Usage:
@@ -103,9 +108,6 @@ class PerlIO:
         else:
             self._open_file(open_str, 'r')
 
-    def __del__(self):
-        self.close()
-
     def __enter__(self):
         return self
 
@@ -154,7 +156,10 @@ class PerlIO:
         return self._proc.stderr
 
     def close(self):
-        self._fo.close()
+        if self._proc == None:
+            self._fo.close()
+        else:
+            self._proc.communicate()
 
 
 class TestPerlIO(unittest.TestCase):
@@ -224,7 +229,7 @@ class TestPerlIO(unittest.TestCase):
         pio_rd = PerlIO(file)
         self.assertNotEqual(pio_rd.fo, None)
         for line in pio_rd.fo.xreadlines():
-            line = line.strip()
+            line = line.rstrip()
             expected = ll.pop(0)
             self.assertEqual(line, expected)
         os.remove(file)
